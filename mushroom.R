5912132056ba02d6ac6a81ca2136197a58d8b48e
Tristan de Boer <info@tristandeboer.nl>
2017-12-13 11:13:09 +0100

    Added Confusion Matrix calculations

--- mushroom.R
+++ mushroom.R
@@ -4,7 +4,6 @@ library("gRain")
 # Import Mushroom Dataset
 mushroom_data <- data.frame(mushroom)
 
-
 ## 80% of the sample size
 sample_size <- floor(0.80 * nrow(mushroom_data))
 
@@ -24,25 +23,22 @@ dgf <- ~cap.shape:class + cap.surface:class + cap.color:class + bruises:class +
   veil.type:class + veil.color:class + ring.number:class + ring.type:class + spore.print.color:class +
   population:class + habitat:class
 
-#dgf <- ~class:cap.shape + class:cap.surface + class:cap.color + class:bruises +
-#        class:odor + class:gill.attachment + class:gill.spacing + class:gill.size +
-#        class:gill.color + class:stalk.shape + class:stalk.root + class:stalk.surface.above.ring +
-#        class:stalk.surface.below.ring + class:stalk.color.above.ring + class:stalk.color.below.ring +
-#        class:veil.type + class:veil.color + class:ring.number + class:ring.type + class:spore.print.color +
-#        class:population + class:habitat
-
 dg <- dag(dgf)
-pp <- extractCPT(train_set, dg)
+# Added smooth parameter to enable dependencies between parameters
+pp <- extractCPT(train_set, dg, smooth=0.5)
 cpp <- compileCPT(pp)
 pn <- grain(cpp)
 
-true = 0
+plot(pn)
 
-print(pp$cap.shape.parray)
-
+# Variable for counting the total amount of true values
+TN = 0
+FN = 0
+TP = 0
+FP = 0
 for (j in 1:nrow(test_set)) {
   # Get test_set entry to determine evidence
-  testdf <- test_set[j,3:23]
+  testdf <- test_set[j,2:23]
   testclass <- test_set[j, 1]
   # Not only the problem of one element list, we also need to change the factor class to character class to be read as evidence
   test <- as.list(vector(length = ncol(testdf)))
@@ -59,16 +55,23 @@ for (j in 1:nrow(test_set)) {
   # Compare true result to 
   if (as.character(maxresult) == "1") {
     maxresult <- "e"
+    if (maxresult == as.character(testclass)) {
+      TP <- TP + 1
+    } else {
+      FP <- FP + 1
+    }
   } else {
     maxresult <- "p"
-  }
-  
-  if (maxresult == as.character(testclass)) {
-    true <- true + 1
+    if (maxresult == as.character(testclass)) {
+      TN <- TN + 1
+    } else {
+      FN <- FN + 1
+    }
   }
 }
 
-print(true/nrow(test_set))
-
-
-
+print(TP)
+print(FP)
+print(FN)
+print(TN)
+print(TN+TP+FP+FN)
